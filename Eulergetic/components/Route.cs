d9cd567f7027d3580ff8c384b7ca5e77662edd4d
Sean Munson <1457924+seanmunson@users.noreply.github.com>
2021-04-26 09:00:18 -0400

    Add route validation

--- Eulergetic/components/Route.cs
+++ Eulergetic/components/Route.cs
@@ -9,6 +9,36 @@ namespace Eulergetic.components
     /// </summary>
     class Route
     {
+        private LinkedList<Edge> routeEdges;
+        // superclass of linked list
+        public Route(Edge [] arr = null)
+        {
+            this.routeEdges = new LinkedList<Edge>();
+            foreach (Edge a in arr)
+            {
+                this.routeEdges.AddLast(a);
+            }
+
+        }
+        /// <summary>
+        /// Retuns if the path is connected
+        /// </summary>
+        /// <returns>true if the head of each element (except the last) returns is the same as the tail of the previous.</returns>
+        public bool isValid()
+        {
+
+            var retval = true;
+
+            var a = routeEdges.First;
+
+            while (a != routeEdges.Last)
+            {
+                if (a.Value.v2 != a.Next.Value.v1) return false;
+                a = a.Next;
+            }
+            return true;
+        }
     }
+    
     
 }
